6c9be4fc96b4bc5e674011aa36e7655b77680a27
Abubakri Sherif <sherif.abubakri@shuttlers.ng>
2023-07-08 00:07:15 +0100

    button to change pairs commented out

--- src/widget/period-bar/index.tsx
+++ src/widget/period-bar/index.tsx
@@ -68,7 +68,7 @@ const PeriodBar: Component<PeriodBarProps> = props => {
           <path d="M192.037 287.953h640.124c17.673 0 32-14.327 32-32s-14.327-32-32-32H192.037c-17.673 0-32 14.327-32 32s14.327 32 32 32zM832.161 479.169H438.553c-17.673 0-32 14.327-32 32s14.327 32 32 32h393.608c17.673 0 32-14.327 32-32s-14.327-32-32-32zM832.161 735.802H192.037c-17.673 0-32 14.327-32 32s14.327 32 32 32h640.124c17.673 0 32-14.327 32-32s-14.327-32-32-32zM319.028 351.594l-160 160 160 160z"/>
         </svg>
       </div>
-      <Show when={props.symbol}>
+      {/* <Show when={props.symbol}>
         <div
           class="symbol"
           onClick={props.onSymbolClick}>
@@ -77,7 +77,7 @@ const PeriodBar: Component<PeriodBarProps> = props => {
           </Show>
           <span>{props.symbol.shortName ?? props.symbol.name ?? props.symbol.ticker}</span>
         </div>
-      </Show>
+      </Show> */}
       {
         props.periods.map(p => (
           <span
